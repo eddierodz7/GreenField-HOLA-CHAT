5f70227de9d0b468e9c906285c812fd98674ec54
SthefaniaRodriguezL <stherod13@gmail.com>
2018-09-12 10:27:14 -0500

    added to components added to components due to internet coulnt push

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Navbar from './components/Navbar'
+import Chat from './components/chat/Chat'
+
+class App extends React.Component {
+render() {
+  return(
+    <React.Fragment>
+       <Navbar/>
+       <Chat/>
+       </React.Fragment>
+  );
+  }
+}
+
+ReactDOM.render(<App/>document.getElementsByClassName('root'));
