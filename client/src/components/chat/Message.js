b2ebf51bf4fff98f4a601e4bd8310bbf3f1782b8
fatimaavilesfa <fatimaaviles1612@gmail.com>
2018-09-12 15:59:14 -0500

    added text/image

--- client/src/components/chat/Message.js
+++ client/src/components/chat/Message.js
@@ -7,7 +7,17 @@ export default const Message = (props) => {
           <p>{props.message.username}</p>
         </div>
 
-      
+        <div className='data'>
+          {props.message.message.type === 'message' ? (
+            <div className='text'>
+              {props.message.message.text}
+            </div>
+          ):(
+            <div className='image'>
+              <img src={props.message.message.url} alt=''/>
+            </div>
+          )}
+        </div>
       </div>
     )
 };
